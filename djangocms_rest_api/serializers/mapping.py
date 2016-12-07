b40073ef06c460e5ff800757cc82b0b76cac447a
Daniel Kirov <daniel.kirov@panmac022.office.pancentric.net>
2016-12-07 09:44:53 +0000

    remove unused serialisers, we do not need them anymore.

--- djangocms_rest_api/serializers/mapping.py
+++ djangocms_rest_api/serializers/mapping.py
@@ -5,12 +5,7 @@ from cmsplugin_filer_image.cms_plugins import FilerImagePlugin
 
 from djangocms_rest_api.serializers.filerimageplugin_serializer import FilerImagePluginSerializer
 
-from positive.serializers import PromoSerializer, TextPluginSerializer
-from positive.cms_plugins import PromoPlugin
-from djangocms_text_ckeditor.cms_plugins import TextPlugin
 
 serializer_class_mapping = {
     FilerImagePlugin: FilerImagePluginSerializer,
-    PromoPlugin: PromoSerializer,
-    # TextPlugin: TextPluginSerializer,
 }
